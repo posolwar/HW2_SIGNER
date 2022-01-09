440979f533c703ad965278ee1c1609136969c859
gorkostya <gorkostya@intersvyaz.net>
2022-01-09 22:57:27 +0500

    тесты проходятся

--- signer.go
+++ signer.go
@@ -1,61 +1,115 @@
 package main
 
 import (
-	"fmt"
 	"sort"
+	"strconv"
 	"strings"
+	"sync"
 )
 
-func ExecutePipeline(job ...job) {
-	var in, out chan interface{}
-	for _, fnc := range job {
-		fnc(in, out)
+const (
+	maxTh int = 6
+)
+
+func ExecutePipeline(jobs ...job) {
+	in := make(chan interface{})
+	wg := &sync.WaitGroup{}
+	defer wg.Wait()
+
+	for _, soloJob := range jobs {
+		wg.Add(1)
+		out := make(chan interface{})
+		go doJob(in, out, wg, soloJob)
+		in = out
 	}
+}
+
+func doJob(in, out chan interface{}, wg *sync.WaitGroup, soloJob job) {
+	defer wg.Done()
+	defer close(out)
+	soloJob(in, out)
+}
+
+func goroutCrc32(data string, outStrChan chan string) {
+	outStrChan <- DataSignerCrc32(data)
 }
 
 func SingleHash(in, out chan interface{}) {
-	rawData := <-in
+	mu := &sync.Mutex{}
+	wg := &sync.WaitGroup{}
+	defer wg.Wait()
 
-	switch typeData := rawData.(type) {
-	case string:
-		out <- DataSignerCrc32(typeData) + "~" + DataSignerCrc32(DataSignerMd5(typeData))
-	default:
-		out <- fmt.Errorf("Unexpected data type. Expected string, received %T", typeData)
+	for v := range in {
+		wg.Add(1)
+		go singleHashJob(v.(int), wg, mu, out)
 	}
-	return
+}
+
+func singleHashJob(value int, wg *sync.WaitGroup, mu *sync.Mutex, out chan interface{}) {
+	defer wg.Done()
+
+	crc32one := make(chan string)
+	crc32two := make(chan string)
+	data := strconv.Itoa(value)
+
+	go goroutCrc32(data, crc32one)
+
+	mu.Lock()
+	md5 := DataSignerMd5(data)
+	mu.Unlock()
+
+	go goroutCrc32(md5, crc32two)
+
+	crc32 := <-crc32one
+	md5crc32 := <-crc32two
+
+	out <- crc32 + "~" + md5crc32
 }
 
 func MultiHash(in, out chan interface{}) {
-	rawData := <-in
-	hashResult := strings.Builder{}
-	hashResult.Grow(6)
+	//	start := time.Now()
+	wg := &sync.WaitGroup{}
+	defer wg.Wait()
 
-	switch typeData := rawData.(type) {
-	case string:
-		for i := 0; i <= 5; i++ {
-			hashResult.WriteString(DataSignerCrc32(fmt.Sprintf("%d%s", i, typeData)))
-		}
-		out <- hashResult.String()
-	default:
-		out <- fmt.Errorf("Unexpected data type. Expected string, received %T", typeData)
+	for v := range in {
+		wg.Add(1)
+		go multiHashJob(v.(string), wg, out)
 	}
-	return
+
+	//	fmt.Printf("time from start MultiHash: %v \n", time.Since(start))
+}
+
+func multiHashJob(value string, wg *sync.WaitGroup, out chan interface{}) {
+	defer wg.Done()
+
+	muInJob := &sync.Mutex{}
+	wgInJob := &sync.WaitGroup{}
+	hashResult := make([]string, maxTh)
+
+	wgInJob.Add(maxTh)
+
+	for i := 0; i < maxTh; i++ {
+		data := strconv.Itoa(i) + value
+		go func(data string, numerator int, results []string, wgInJob *sync.WaitGroup, muInJob *sync.Mutex) {
+			results[numerator] = DataSignerCrc32(data)
+			wgInJob.Done()
+		}(data, i, hashResult, wgInJob, muInJob)
+	}
+	wgInJob.Wait()
+	out <- strings.Join(hashResult[:], "")
 }
 
 func CombineResults(in, out chan interface{}) {
-	strArray := make([]string, 0, 2)
+	//	start := time.Now()
 
-loop:
-	select {
-	case data := <-in:
-		str, _ := data.(string)
-		strArray = append(strArray, str)
-	default:
-		sort.Strings(strArray)
-		out <- strings.Join(strArray, "_")
-		break loop
+	resSlice := make([]string, 0, 6)
+
+	for v := range in {
+		resSlice = append(resSlice, v.(string))
 	}
 
-	// sort.Strings(hashes)
-	// return strings.Join(hashes, "_")
+	sort.Strings(resSlice)
+	out <- strings.Join(resSlice[:], "_")
+
+	//	fmt.Printf("time from start CombineResults: %v \n ", time.Since(start))
 }
